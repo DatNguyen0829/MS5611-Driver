595296063c9bc5dddcabd3406b74eb1acffcce10
DanNguyen <bidat2007@gmail.com>
2026-01-13 16:51:13 -0800

    Driver needs testing

--- main/ms5611.c
+++ main/ms5611.c
@@ -30,17 +30,17 @@ int32_t TEMP;
 int64_t OFF;
 int64_t SENS;
 int32_t P;
-int32_t T2 = 0;
-int64_t OFF2 = 0;
-int64_t SENS2 = 0;
+int32_t T2;
+int64_t OFF2;
+int64_t SENS2;
 
 static void i2c_master_init(i2c_master_bus_handle_t *bus_handle, i2c_master_dev_handle_t *dev_handle)
 {
     //Set I2C master bus configuration
     i2c_master_bus_config_t bus_config = {
         .i2c_port = I2C_MASTER_NUM,
-        .sda_io_num = GPIO_NUM_21,
-        .scl_io_num = GPIO_NUM_22,
+        .sda_io_num = I2C_MASTER_SDA_IO,
+        .scl_io_num = I2C_MASTER_SCL_IO,
         .clk_source = I2C_CLK_SRC_DEFAULT,
         .glitch_ignore_cnt = 7,
         .flags.enable_internal_pullup = true,  
@@ -58,6 +58,13 @@ static void i2c_master_init(i2c_master_bus_handle_t *bus_handle, i2c_master_dev_
     //Initialize I2C device handle for MS5611 sensor
     ESP_ERROR_CHECK(i2c_master_bus_add_device(*bus_handle, &dev_config, dev_handle));
 
+}
+
+static void ms5611_reset(i2c_master_dev_handle_t dev_handle)
+{
+    uint8_t rst = 0x1E;
+    ESP_ERROR_CHECK(i2c_master_transmit(dev_handle, &rst, 1, -1));
+    vTaskDelay(pdMS_TO_TICKS(3)); // datasheet: ~2.8ms max
 }
 
 static void ms5611_read_prom(i2c_master_dev_handle_t dev_handle)
@@ -92,11 +99,14 @@ static void ms5611_read_conversion(i2c_master_dev_handle_t dev_handle, uint8_t c
 }
 
 static void ms5611_calculateTemperature()
-{
+{   
     dT = D2 - (((uint32_t) prom_data[5])<<8);
     int64_t calcTemp = 2000 + ((int64_t)dT * prom_data[6]) / (1LL << 23);
     TEMP = (int32_t) calcTemp;
 
+    T2 = 0;
+    OFF2 = 0;
+    SENS2 = 0;
     //Compensate for low Temp
     if(TEMP<2000)
     {
@@ -122,7 +132,7 @@ static void ms5611_calculatePressure()
     OFF -= OFF2;
     SENS -= SENS2;
     //Final Pressure Result
-    P = (D1*(SENS/(1LL << 21)) - OFF)/(1LL << 15);
+    P = ((D1*(SENS)/(1LL << 21)) - OFF)/(1LL << 15);
 }
 
 void app_main(void)
@@ -134,6 +144,8 @@ void app_main(void)
     i2c_master_init(&bus_handle, &dev_handle);
     ESP_LOGI(TAG, "I2C Master initialized and MS5611 device added.");
 
+    //Reset ms5611 sensor
+    ms5611_reset(dev_handle);
     //Read device Prom values
     ms5611_read_prom(dev_handle);
 
@@ -146,7 +158,7 @@ void app_main(void)
         ms5611_calculateTemperature();
         ms5611_calculatePressure();
 
-        printf("The temparture is %d", TEMP);
+        printf("The temparture is %d\n", TEMP);
         prinf("The pressure is %d", P);
     }
 
